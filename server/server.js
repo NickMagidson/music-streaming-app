c958e994b9406d84470fc2ef1b48abb79cce96f9
Nick Magidson <Nickmagi24@gmail.com>
2022-05-23 10:29:38 -0400

    Server login routing and authorization

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api');
+
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '43e436c5a3504f8194e9da6af1ed7dba',
+        clientSecret: '52aa0879b39049b8af4167356589318b'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    })
+    .catch(() => {
+        res.sendStatus(400)
+    })
+
+})
