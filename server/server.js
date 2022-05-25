ff5254aea929d36de07a708ca1708136d9b49057
Nick Magidson <Nickmagi24@gmail.com>
2022-05-25 18:08:28 -0400

    useAuth and server setup

--- server/server.js
+++ server/server.js
@@ -4,22 +4,23 @@ const SpotifyWebApi = require('spotify-web-api');
 const app = express();
 
 app.post('/login', (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '43e436c5a3504f8194e9da6af1ed7dba',
-        clientSecret: '52aa0879b39049b8af4167356589318b'
-    })
+	const code = req.body.code
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: 'http://localhost:3000',
+		clientId: '43e436c5a3504f8194e9da6af1ed7dba',
+		clientSecret: '52aa0879b39049b8af4167356589318b'
+	})
 
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        })
-    })
-    .catch(() => {
-        res.sendStatus(400)
-    })
-
+	spotifyApi.authorizationCodeGrant(code).then(data => {
+		res.json({
+			accessToken: data.body.access_token,
+			refreshToken: data.body.refresh_token,
+			expiresIn: data.body.expires_in
+		})
+	})
+	.catch(() => {
+		res.sendStatus(400)
+	})
 })
+
+app.listen(3001)
